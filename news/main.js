7c183dd7de9ee7fd7a7ce7c75f51c2dab84ec059
alim <dahye_a@naver.com>
2024-07-10 02:58:50 +0900

    fetch error test

--- news/main.js
+++ news/main.js
@@ -6,7 +6,7 @@ let category = '';
 
 const getLatestNews = async () => {
   // const url = new URL(`https://newsapi.org/v2/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}`); // 참고: https://developer.mozilla.org/ko/docs/Web/API/URL
-  url = new URL(`https://alim-js-study.netlify.app/news/top-headlines?country=${COUNTRY}`);
+  url = new URL(`https://alim-js-study.netlify.app/top-headlines?country=${COUNTRY}`);
   // url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
   const response = await fetch(url);
   const data = await response.json();
@@ -30,7 +30,7 @@ const setPhrase = (text, len) => {
 
 const categoryFilter = async (category) => {
   // url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?category=${category.toLowerCase()}`);
-  url = new URL(`https://alim-js-study.netlify.app/news/top-headlines?country=${COUNTRY}&category=${category.toLowerCase()}`)
+  url = new URL(`https://alim-js-study.netlify.app/top-headlines?country=${COUNTRY}&category=${category.toLowerCase()}`)
   const response = await fetch(url);
   const data = await response.json();
   if(data.articles.length !== 0){
@@ -44,7 +44,7 @@ const render = () => {
   const newsHTML = newsList.map((news) => `
       <div class="news">
         <p class="image-area">
-          <img src=${ news.urlToImage ? news.urlToImage : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU'} alt="" class="news-img-size">
+          <img src=${ news.urlToImage ? news.urlToImage : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU' } alt="" class="news-img-size">
         </p>
         <div class="text-area">
           <h2>${news.title}</h2>
