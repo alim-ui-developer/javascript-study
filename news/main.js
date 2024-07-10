6e283f6fa57e2658f68d7e1af5219c6bc43a2e4c
alim <dahye_a@naver.com>
2024-07-11 00:12:28 +0900

    모바일 메뉴, 검색창 추가

--- news/main.js
+++ news/main.js
@@ -1,13 +1,13 @@
 const COUNTRY = 'us';
-// const API_KEY = '1b17a188b790486989053206c1e446ef';
-const API_KEY = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`;
+const API_KEY = '1b17a188b790486989053206c1e446ef';
+// const API_KEY = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`;
 let url = '';
 let newsList = [];
 let category = '';
 
 const getLatestNews = async () => {
-  // url = new URL(`https://newsapi.org/v2/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}`); // 참고: https://developer.mozilla.org/ko/docs/Web/API/URL
-  url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=${COUNTRY}`)
+  url = new URL(`https://newsapi.org/v2/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}`); // 참고: https://developer.mozilla.org/ko/docs/Web/API/URL
+  // url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=${COUNTRY}`)
   const response = await fetch(url);
   const data = await response.json();
 
@@ -29,8 +29,8 @@ const setPhrase = (text, len) => {
 }
 
 const categoryFilter = async (category) => {
-  // url = new URL(`https://newsapi.org/v2/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}&category=${category.toLowerCase()}`)
-  url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?category=${category.toLowerCase()}`);
+  url = new URL(`https://newsapi.org/v2/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}&category=${category.toLowerCase()}`)
+  // url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?category=${category.toLowerCase()}`);
   const response = await fetch(url);
   const data = await response.json();
   if(data.articles.length !== 0){
@@ -44,16 +44,21 @@ const render = () => {
   const newsHTML = newsList.map((news) => `
       <div class="news">
         <p class="image-area">
-          <img src=${ news.urlToImage ? news.urlToImage : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU' } alt="" class="news-img-size">
+          <img
+            src=${ news.urlToImage}
+            onError="this.onerror=null; this.src='https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png'"
+            alt=""
+            class="news-img-size"
+          />
         </p>
         <div class="text-area">
           <h2>${news.title}</h2>
           <p>
             ${ setPhrase(news.description, 200) }
           </p>
-          <ul>
+          <ul class="copyright">
             <li>${ news.source.name ? news.source.name : 'no source' }</li>
-            <li>${ moment(news.publishedAt).format('llll') }</li>
+            <li>${ moment(news.publishedAt).fromNow() }</li>
           </ul>
         </div>      
     </div>
